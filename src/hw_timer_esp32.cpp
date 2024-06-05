3117b467396363f9bef192ef7245ec1456b4dbe1
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-06-05 11:44:38 +0200

    Arduino 3 compatibility See: https://docs.espressif.com/projects/arduino-esp32/en/latest/migration_guides/2.x_to_3.0.html

--- src/hw_timer_esp32.cpp
+++ src/hw_timer_esp32.cpp
@@ -26,6 +26,11 @@ const static int TIMER_ID = 0;
 static hw_timer_t* timer = nullptr;
 
 void timerInit(void (*callback)()) {
+#if ESP_IDF_VERSION_MAJOR >= 5
+  timer = timerBegin(1000000);
+  timerWrite(timer, 0);
+  timerAttachInterrupt(timer, callback);
+#else
   // Use 1st timer of 4 (counted from zero).
   // Set 80 divider for prescaler (see ESP32 Technical Reference Manual for more
   // info), count up. The counter starts to increase its value.
@@ -34,25 +39,37 @@ void timerInit(void (*callback)()) {
   timerWrite(timer, 0);
 
   timerAttachInterrupt(timer, callback, false);
+#endif
 }
 
 void ARDUINO_ISR_ATTR startTimerAndTrigger(uint32_t delay) {
+#if ESP_IDF_VERSION_MAJOR >= 5
+  timerRestart(timer);
+  timerAlarm(timer, delay, false, 0);
+#else
   timerWrite(timer, 0);
   timerAlarmWrite(timer, delay, false);
   timerAlarmEnable(timer);
   timerStart(timer);
+#endif
 }
 
 void ARDUINO_ISR_ATTR setAlarm(uint32_t delay) {
+#if ESP_IDF_VERSION_MAJOR >= 5
+  timerAlarm(timer, delay, false, 0);
+#else
   timerAlarmWrite(timer, delay, false);
 
   // On core v2.0.0-2.0.1, the timer alarm is automatically disabled after triggering,
   // so re-enable the alarm
   timerAlarmEnable(timer);
+#endif
 }
 
 void ARDUINO_ISR_ATTR stopTimer() {
+#if ESP_IDF_VERSION_MAJOR < 5
   timerStop(timer);
+#endif
 }
 
 #endif  // END ESP32
